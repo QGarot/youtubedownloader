5d24ede34e7b68c336e242e2354013491c44b4bb
Tiig3r <leplusbeauswing@gmail.com>
2022-07-22 22:23:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# :musical_note: YoutubeDownloader :musical_note:
+
+Téléchargez facilement vos musiques depuis YouTube !
+
+## Installation
+- Installer Python : https://www.python.org/downloads/ (bien cocher *Add Python to PATH* au début de l'installation)
+- Télécharger **YoutubeDownloader** [en cliquant ici](https://github.com/QGarot/youtubedownloader/archive/refs/heads/master.zip) puis extraire le dossier **youtubedownloader-master** (sur le bureau par exemple)
+- Lancer l'invite de commande en tapant "*cmd*" dans la barre de recherche Windows, puis éxecuter :
+```
+pip install pytube
+```
+et
+```
+pip install music_tag
+```
+:heavy_check_mark: L'installation est terminée !
+
+## Utilisation
+
+- Lancer le fichier **main.py** et se laisser guider !
+
+1. Entrer le titre de la musique souhaitée, 20 propositions seront alors affichées.
+2. Entrer le numéro de celle qui convient (par exemple la version officielle)
+3. La musique sera alors téléchargée sous le format MP3 dans le dossier **musiques** !
+4. Taper "*y*" si d'autres musiques sont à télécharger, sinon taper une autre touche quelconque et le programme se fermera.
+
+- Les musiques sont téléchargées dans le dossier **musiques** généré par le programme
---
+++ main.py
@@ -0,0 +1,53 @@
+from pytube import Search
+from urllib.request import urlopen
+import music_tag
+import os
+
+
+path = os.getcwd() + "\\musiques"
+
+
+def run1():
+    active = True
+    while active:
+        search = str(input("Enter title of a song >>> "))
+        videos_ytb = Search(search).results
+        print("We found : ")
+        for i in range(len(videos_ytb)):
+            print(str(i + 1) + ") " + videos_ytb[i].title + " de : " + videos_ytb[i].author)
+
+        n = int(input("Which one do you want to save? >>> "))
+        videos_ytb = videos_ytb[n - 1]
+        download_mp3(videos_ytb)
+        active = str(input("Do you want to download an other music? >>> ")) == "y"
+        print("__________________________________________________________")
+
+
+def edit_mp3_data(video_ytb, file_name):
+    thumbnail_url = video_ytb.thumbnail_url
+    artist = video_ytb.author
+    f = music_tag.load_file(path + "\\" + file_name)
+    f["artist"] = artist
+    img = urlopen(thumbnail_url).read()
+    f["artwork"] = img
+    f.save()
+
+
+def download_mp3(video_ytb):
+    video = video_ytb.streams.filter(only_audio=True).first()
+
+    # Télécharge le fichier et retourne le path sous forme de chaîne de caractères.
+    out_file_name = video.download(output_path=path)
+
+    # .mp4 to .mp3
+    base = os.path.splitext(out_file_name)[0]
+    new_file_name = base + ".mp3"
+    os.rename(out_file_name, new_file_name)
+
+    edit_mp3_data(video_ytb, os.path.basename(new_file_name))
+
+    # Success
+    print(video_ytb.title + " has been downloaded! :D")
+
+
+run1()
